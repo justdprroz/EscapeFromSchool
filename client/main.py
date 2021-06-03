1c63a4876d98ce83376a3e1d1c7c5cdb38128bc3
bneurbuier <nbtuibdfjnb>
2021-06-03 16:38:31 +0000

    Added client

---
+++ client/main.py
@@ -0,0 +1,3 @@
+import os
+a = os.system('whiptail --title "Title of the game" --yesno "Are you John?" 10 60')
+b = os.system('whiptail --title "Is it Tuesday?" --yesno "Is today Tuesday?" --yes-button "Really" --no-button "No Really" --cancel-button "rjwiqj" 8 78')
