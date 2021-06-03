7c7c6b6f9771d7e127bced324a3cc50d6bb30341
justdprroz <dmitri.proskuriakov@gmail.com>
2021-06-03 13:13:00 +0300

    Updated logic

--- server/logic.py
+++ server/logic.py
@@ -3,24 +3,29 @@ import json
 class plotDriver:
 	def loadPlotFromJSON(self):
 		with open("plot.json", "r") as plotFile:
-			self.plotData = json.load(plotFile)
+			self.plot = json.load(plotFile)
+			
 	def __init__(self):
-		self.plotData = None
-		self.currentNode = None
-		loadPlotFromJSON()
+		self.plot = None
+		self.loadPlotFromJSON()
+		self.isEnded = False
 
-	def doTurn(index):
-		variants = list(self.currentNode.keys())
+	def doTurn(self, index):
+		if self.isEnded:
+			return 0
+		variants = list(self.plot.keys())
 		if (index < 0 or index > len(variants)):
 			return 0
-		self.currentNode = self.currentNode[variants[index]]
-		if isinstance(self.currentNode, str):
-			return 2
-		if isinstance(self.currentNode, dict):
+		self.currentNode = self.plot[variants[index]]
+		if isinstance(self.plot, str):
+			self.isEnded = True
+			return 1
+		if isinstance(self.plot, dict):
 			return 1
 
-	def getVariant():
-		return self.currentNode
-
-if "__name__" == "__main__":
-	print("hello")
+	def getVariant(self):
+		if not self.isEnded:
+			variants = list(self.plot.keys())
+		if self.isEnded:
+			variants = self.plot
+		return variants
